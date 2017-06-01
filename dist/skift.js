db151d9389d28fc193191ef8c97606b7223ffa82
Eduard Kyvenko <eky@trustpilot.com>
2017-06-01 13:40:55 +0200

    Change the distilled files

--- dist/skift.js
+++ dist/skift.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
 	typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
-	(factory((global['node-splittestingtool'] = global['node-splittestingtool'] || {}),global.jQuery));
+	(factory((global.skift = global.skift || {}),global.jQuery));
 }(this, (function (exports,$$1) { 'use strict';
 
 $$1 = 'default' in $$1 ? $$1['default'] : $$1;

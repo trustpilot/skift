b694ea0fa34697dd340f6b1598a21892ebabc172
Eduard Kyvenko <eky@trustpilot.com>
2017-06-07 09:07:41 +0200

    Update the distributive

--- types/main.d.ts
+++ types/main.d.ts
@@ -18,18 +18,15 @@ export declare const config: {
     userSessionDaysToLive: number;
     cookieName: string;
 };
-export declare function initialize(): void
-export declare function getUserAgentInfo(): UserAgentInfo
-export declare function getTest(name: string): SplitTest
-export declare function canRunTest(test: SplitTest): boolean
-export declare function create(name: string): SplitTest
-export declare function getTestVariant(testName: string): string
-export declare function hasTestVariant(
-    testName: string,
-    variant: string
-): boolean
-export declare function setTestVariant(testName: string, variant: string): void
-export declare function reset(): void
+export declare function initialize(): void;
+export declare function getUserAgentInfo(): UserAgentInfo;
+export declare function getTest(name: string): SplitTest;
+export declare function canRunTest(test: SplitTest): boolean;
+export declare function create(name: string): SplitTest;
+export declare function getTestVariant(testName: string): string;
+export declare function hasTestVariant(testName: string, variant: string): boolean;
+export declare function setTestVariant(testName: string, variant: string): void;
+export declare function reset(): void;
 export declare namespace ui {
     function show(testsList: SplitTest[]): void;
     function hide(): void;

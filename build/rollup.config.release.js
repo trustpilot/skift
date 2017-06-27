0977fd3afab3e03d024adc388bb60298d3d8b8ae
Eduard Kyvenko <eky@trustpilot.com>
2017-06-28 00:40:44 +0300

    Removing rollup dependencies, config and scripts

--- build/rollup.config.release.js
+++
@@ -1,8 +0,0 @@
-import rollupConfig from './rollup.config.debug';
-import uglify from 'rollup-plugin-uglify';
-
-rollupConfig.dest = rollupConfig.dest.replace(/\.js$/, '.min.js');
-
-rollupConfig.plugins.push(uglify());
-
-export default rollupConfig;

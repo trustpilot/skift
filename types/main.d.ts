4a70b69592001c67578b3f997ccaf9e2d651326d
Thomas Thiebaud <thiebaud.tom@gmail.com>
2018-06-25 22:02:41 +0200

    Remove old types

--- types/main.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function initialize(): void;
-export declare function getCurrentTestVariation(testName: string): string;
-export declare function setCurrentTestVariation(testName: string, variation: string): void;
-export declare function reset(): void;
-export declare const getUserAgentInfo: any;
--- types/usersessioncookiepersister.d.ts
+++
@@ -1,3 +0,0 @@
-import { UserSessionPersister } from './config';
-declare const persister: UserSessionPersister;
-export default persister;

dc8d2425580060119212d9af954f895b590b0d95
Thomas Thiebaud <thiebaud.tom@gmail.com>
2018-07-02 15:56:29 +0200

    chore: update webpack

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
   entry: {
     skift: './src/index.ts'
   },
@@ -14,39 +16,26 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js'],
-    modules: ['node_modules']
   },
   module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ],
-        exclude: [/\.e2e\.ts$/]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'css-to-string-loader'
-          },
-          {
-            loader: 'css-loader'
-          }
-        ]
-      }
-    ]
+    rules: [{
+      test: /\.ts$/,
+      use: [{
+        loader: 'ts-loader'
+      }],
+      exclude: [/\.e2e\.ts$/]
+    }, {
+      test: /\.css$/,
+      use: [{
+        loader: 'css-to-string-loader'
+      }, {
+        loader: 'css-loader'
+      }]
+    }]
   },
-  plugins: [],
-  node: {
-    global: true,
-    process: false,
-    crypto: 'empty',
-    module: false,
-    clearImmediate: false,
-    setImmediate: false
-  }
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
 };

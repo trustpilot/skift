41d14403e6b9011b709996e4d2901215db91b5ea
Thomas Thiebaud <thiebaud.tom@gmail.com>
2018-07-03 13:30:15 +0200

    fix: remove types from git history

--- types/ui.d.ts
+++
@@ -1,7 +0,0 @@
-import { BehavioralSubject } from './behavioral-subject';
-import { SplitTest } from './splittest';
-import { UserAgentInfo } from './useragentinfo';
-export declare const uiFactory: (tests: BehavioralSubject<SplitTest[]>, reset: () => void, getCurrentTestVariation: (testName: string) => string, getUserAgentInfo: () => UserAgentInfo) => {
-    show: () => void;
-    hide: () => void;
-};

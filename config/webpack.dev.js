403c41c1cb6768857452191e7f27afcc7ad10dad
Thomas Thiebaud <thiebaud.tom@gmail.com>
2018-07-05 09:26:59 +0200

    feat: remove UI from production bundle

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -26,7 +26,7 @@ module.exports = {
         loader: 'ts-loader'
       }],
       exclude: [/\.e2e\.ts$/]
-    }, {
+     }, {
       test: /\.css$/,
       use: [{
         loader: 'css-to-string-loader'
